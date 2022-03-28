c7a078d43dafbd1da6ab5bc4e6b5877b2b9a6951
Ariana Tran <atran2@mail.umw.edu>
2022-03-28 18:00:08 -0400

    fix something

--- msgq.c
+++ msgq.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "msgq.h"
 #include <pthread.h>
 #include "zemaphore.h"
 
 struct msgq *msgq_init(int num_msgs) {
-
+    struct msgq *m = malloc(sizeof(struct msgq));
+    return m;
 }
 int msgq_send(struct msgq *mq, char *msg) {
 
